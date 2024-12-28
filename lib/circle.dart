d6f5a2806c80e3c0168995ef67f5dd763eaf8d3d
zainab-frfr <135985226+zainab-frfr@users.noreply.github.com>
2024-12-28 12:10:37 +0500

    q2 done

--- lib/circle.dart
+++ lib/circle.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
 class MyCircle extends StatelessWidget {
-  final Color colorOne;
-  final Color colorTwo;
-  const MyCircle({super.key, required this.colorOne, required this.colorTwo});
+  final Color colorbg;
+  final Color colorText;
+  final double diameter;
+  final String text;
+  const MyCircle({super.key, required this.colorbg, required this.text, required this.diameter, required this.colorText});
 
   @override
   Widget build(BuildContext context) {
@@ -11,14 +13,12 @@ class MyCircle extends StatelessWidget {
       child: Stack(
         children: [
           Container(
-            width: 30,
-            height: 30,
-            color: colorOne,
-          ),
-          Container(
-            width: 30,
-            height: 15,
-            color: colorTwo,
+            width: diameter,
+            height: diameter,
+            color: colorbg,
+            child: Center(
+              child: Text(text, style: TextStyle(color: colorText, fontWeight: FontWeight.bold),),
+            ),
           ),
         ],
       ),
